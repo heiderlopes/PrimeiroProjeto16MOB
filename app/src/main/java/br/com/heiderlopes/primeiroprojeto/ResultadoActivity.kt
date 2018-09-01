2a5b33b2f83be95dac3cce4ab210000a00040a56
Heider Lopes <heidertreinamentos@gmail.com>
2018-09-01 11:58:31 -0300

    Formatacao do layout de resultado

--- app/src/main/java/br/com/heiderlopes/primeiroprojeto/ResultadoActivity.kt
+++ app/src/main/java/br/com/heiderlopes/primeiroprojeto/ResultadoActivity.kt
@@ -2,7 +2,10 @@ package br.com.heiderlopes.primeiroprojeto
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.widget.Toast
 import br.com.heiderlopes.primeiroprojeto.utils.ConstantesExtra
+import kotlinx.android.synthetic.main.activity_resultado.*
 
 class ResultadoActivity : AppCompatActivity() {
 
@@ -13,6 +16,18 @@ class ResultadoActivity : AppCompatActivity() {
         val peso = intent.extras.getString(ConstantesExtra.KEY_PESO)
         val altura = intent.extras.getString(ConstantesExtra.KEY_ALTURA)
 
+        calcularIMC(peso.toDouble(), altura.toDouble())
+    }
 
+    private fun calcularIMC(peso: Double, altura: Double) {
+        var imc = peso / (altura * altura)
+        imc = 22.0
+
+        
+    }
+
+    private fun setImagem(resourceId: Int) {
+        ivIMC.setImageDrawable(
+                ContextCompat.getDrawable(this, resourceId))
     }
 }
